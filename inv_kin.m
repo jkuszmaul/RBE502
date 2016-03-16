fa71dfdf6540d3d632cc26092a894d83ee539db5
James Kuszmaul <jabukuszmaul+collab@gmail.com>
2016-03-16 13:51:24 -0400

    Made inverse kinematics more usable.

--- inv_kin.m
+++ inv_kin.m
@@ -1,22 +1,30 @@
-function [ret, link_cts, elbow_pts, hex_pts] = inv_kin()
+function [ret, sym_theta, plots] = inv_kin()
+  % Returns the inverse kinematics function handle, which
+  % takes an end-effector homogeneous transformation matrix.
   ret = 0;
-  %H = sym('H', [4, 4]);
-  H = eye(4);
+  H = sym('H', [4, 4]);
+  %H = eye(4);
   H(4, :) = [0 0 0 1];
 
   % Translational component:
-  H(1:3, 4) = [0; 1; -4];
+  %H(1:3, 4) = [0; 0; -3];
 
   % Rotational component:
-  theta = 0.0;
-  H(1:3, 1:3) = [cos(theta) -sin(theta) 0;
-                 sin(theta) cos(theta) 0;
-                 0          0          1];
+  thetaz = 1.0;
+  thetax = 1.5;
+  R = [cos(thetaz) -sin(thetaz) 0;
+       sin(thetaz) cos(thetaz) 0;
+       0          0          1];
+  R = [1          0            0;
+       0 cos(thetax) -sin(thetax);
+       0 sin(thetax) cos(thetax)] * R;
+  %H(1:3, 1:3) = R;
+
   % Various Parameters.
   link_sep = .1; % Distance between links in pairs of link segments.
   long_side = .2; % Distance between pair sets.
   elbow_len = 1; % Length of first link.
-  link_len = 4; % Length of second link.
+  link_len = 3; % Length of second link.
 
   % Begin actually calculating.
   % Get the joint connection locations on the end effector.
@@ -27,35 +35,59 @@ function [ret, link_cts, elbow_pts, hex_pts] = inv_kin()
   elbow_pts = start_constraints(link_sep, long_side, elbow_len, theta);
   % Get the actual link constraints.
   link_cts = simplify(link_constraints(elbow_pts, hex_pts, link_len)) == 0;
-  ret = sym('ret', [6, 1]);
+  sym_theta = sym('sym_theta', [6, 2]);
   for i = 1:6
+    i
     res = solve(link_cts(i), theta(i), 'Real', 1);
+    sym_theta(i, 1:2) = res';
+    continue
     % Choose the result that actually makes sense (abs(theta) < pi/2).
     if (abs(res(1)) < pi / 2)
-      ret(i) = res(1)
+      sym_theta(i) = res(1);
     else
-      ret(i) = res(2);
+      sym_theta(i) = res(2);
     end
   end
 
-  % Do plotting.
-  eval_elb = eval(subs(elbow_pts, theta, ret'));
-  eval_hex = eval(subs(hex_pts, theta, ret'));
-  % Get locations of motor joints.
-  base_hex = eval(link_ends(eye(4), link_sep, long_side))
-  new_plot = zeros(6 * 3, 3);
-  for i = 1:3
-    new_plot(6*i-5, :) = base_hex(2*i-1, :);
-    new_plot(6*i-4, :) = eval_elb(2*i-1, :);
-    new_plot(6*i-3, :) = eval_hex(2*i-1, :);
-    new_plot(6*i-2, :) = eval_hex(2*i, :);
-    new_plot(6*i-1, :) = eval_elb(2*i, :);
-    new_plot(6*i, :) = base_hex(2*i, :);
+  ret = @eval_ret;
+  plots = @plotter;
+
+  function best = eval_ret(H_act)
+    angles = subs(sym_theta, H, H_act);
+    best = eval(angles(:, 1));
+    % Now, always choose the values with the smaller absolute value.
+    for i = 1:6
+      % Only need to change anything if abs(2) < abs(1)
+      if abs(angles(i, 2)) < abs(angles(i, 1))
+        best(i) = eval(angles(i, 2));
+      end
+    end
   end
-  x_plot = new_plot(:, 1);
-  y_plot = new_plot(:, 2);
-  z_plot = new_plot(:, 3);
-  plot3(x_plot, y_plot, z_plot, 'LineStyle', '-', 'Marker', 'o');
+
+  function plotter(H_act, joints)
+    % Do plotting.
+    elbow_pts
+    eval_elb = subs(elbow_pts, theta, joints');
+    eval_hex = subs(subs(hex_pts, theta, joints'), H, H_act);
+    % Get locations of motor joints.
+    base_hex = eval(link_ends(eye(4), link_sep, long_side));
+    new_plot = zeros(6 * 3, 3);
+    for i = 1:3
+      new_plot(6*i-5, :) = base_hex(2*i-1, :);
+      new_plot(6*i-4, :) = eval_elb(2*i-1, :);
+      new_plot(6*i-3, :) = eval_hex(2*i-1, :);
+      new_plot(6*i-2, :) = eval_hex(2*i, :);
+      new_plot(6*i-1, :) = eval_elb(2*i, :);
+      new_plot(6*i, :) = base_hex(2*i, :);
+    end
+    x_plot = new_plot(:, 1);
+    y_plot = new_plot(:, 2);
+    z_plot = new_plot(:, 3);
+    eval_hex(7, :) = eval_hex(1, :); % Makes graph prettier :)
+    plot3(x_plot, y_plot, z_plot, eval_hex(:, 1), eval_hex(:, 2), eval_hex(:, 3),...
+          'LineStyle', '-', 'Marker', 'o');
+  end
+
 end
 
 function pts = link_ends(H, short, long)
@@ -78,7 +110,7 @@ function pts = link_ends(H, short, long)
 end
 
 function cts = link_constraints(elbow_pts, hex_pts, link_len)
-  dist2 = sum((elbow_pts - hex_pts).^2, 2)
+  dist2 = sum((elbow_pts - hex_pts).^2, 2);
   cts = dist2 - link_len^2;
 end
 
